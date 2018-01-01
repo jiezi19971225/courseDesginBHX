5b956cfc603bc647687bd3c7d7deb2a802c246bd
lj <jiezi19971225@163.com>
2018-01-01 18:43:01 +0800

    0.5

--- js/main.js
+++ js/main.js
@@ -64,7 +64,7 @@ function initSupportArray(){
 			for(var k = 0;k < 8;k++){
 				var newI=i + mov[k][0];
 				var newJ=j + mov[k][1];
-				if(newI > 0 && newI < 8 && newJ > 0 && newJ < 8){
+				if(newI >= 0 && newI < 8 && newJ >= 0 && newJ < 8){
 					accessible[i][j]++;
 				} 
 			}
@@ -91,15 +91,15 @@ function generateRandomPositon(){
 	var rrow = Math.floor(Math.random()*8);
 	var rcol = Math.floor(Math.random()*8);
 	var beginStatu = new Statu(rrow,rcol,1);
-	handleNewStatu(beginStatu);
+	handleStatuAccessible(beginStatu);
 	path[rrow][rcol]=1;
 	return beginStatu;
 }
 //记录可行方案
 function recordResolution(){
-	var res=new Array();
+	var res=[];
 	for(var i = 0;i < 8;i++){
-		res[i]=new Array();
+		res[i]=[];
 	}
 	for(var i = 0;i < 8;i++){
 		for(var j = 0;j < 8;j++){
@@ -108,67 +108,119 @@ function recordResolution(){
 	}
 	resolution.push(res);
 }
-//处理新状态的可行性数组
-function handleNewStatu(statu){
+//处理状态的可行性数组
+function handleStatuAccessible(statu){
 	var access = [];
 	var order = [0,1,2,3,4,5,6,7];
 	for(var i = 0;i < 8;i++){
 		var newRow = statu.row + mov[i][0];
 		var newCol = statu.col + mov[i][1];
-		if(newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8){
-			access[i] =  accessible[newRow][newCol];
+		if(newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8 && !path[newRow][newCol]){
+				access[i] =  accessible[newRow][newCol] - 1;
 		}else{
 			access[i] = 0;
 		}
 	}
 	bubbleSort(access,order);
 	var cur = 0;
-	var currentMoveWay = 0;
-	while(order[cur] == 0 && cur < 8){
-		currentMoveWay++;
+	while(access[cur] == 0 && cur < 8){
+		cur++;
 	}
-	statu.currentMoveWay = currentMoveWay;
+	statu.currentMoveWay = cur;
 	statu.order = order;
+
+	if(cur == 8){
+		return false;
+	}
+	return true;
 }
-//解决函数
+//调整可行性
+function adjust(row,col){
+	for(var i = 0;i <8;i++){
+		var newRow = row + mov[i][0]; 
+		var newCol = col + mov[i][1];
+		if(newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8){
+			accessible[newRow][newCol]--;
+		} 
+	}
+}
+function rollBack(row,col){
+	for(var i = 0;i <8;i++){
+		var newRow = row + mov[i][0]; 
+		var newCol = col + mov[i][1];
+		if(newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8){
+			accessible[newRow][newCol]++;
+		} 
+	}
+}
+//生成一组答案
 function solve(){
 	while(!stack.empty()){
 		var currentStatu = stack.top();
 		var haveNextStatu = 0;
 		var currentMoveWay = currentStatu.currentMoveWay;
-		if(currentStatu.step == 60){
-			//recordResolution();
+		if(currentStatu.step == 63){
+			console.log("ok");
 			path[currentStatu.row][currentStatu.col] = 0;
+			rollBack(currentStatu.row,currentStatu.col);
 			stack.pop();
-			continue;
+			return;
 		}
-		for(var i = currentMoveWay;i < 8;i++){
+		for(var i = currentStatu.currentMoveWay;i < 8;i++){
 			moveMay = currentStatu.order[i];
-			var newRow = currentStatu.row + mov[i][0];
-			var newCol = currentStatu.col + mov[i][1];
-			//console.log(newX,newY);
+			var newRow = currentStatu.row + mov[moveMay][0];
+			var newCol = currentStatu.col + mov[moveMay][1];
 			newStatu = new Statu(newRow,newCol,currentStatu.step+1);
-			handleNewStatu(newStatu);
-			stack.push(newStatu);
-			break;
+			
+			if(handleStatuAccessible(newStatu)){
+				haveNextStatu = 1;
+				stack.push(newStatu);
+				adjust(newRow,newCol);
+				path[newRow][newCol] = currentStatu.step + 1;
+				currentStatu.currentMoveWay += 1;
+				break;	
+			}
 		}
 		if(!haveNextStatu){
 			path[currentStatu.row][currentStatu.col] = 0;
+			rollBack(currentStatu.row,currentStatu.col);
 			stack.pop();
 		}
 	}
 }
+function generateGraph(){
+	var chessboard = $(".chessboard");
+	for(var i = 0;i < 8;i++){
+		for(var j=0;j < 8;j++){
+			var gird = $("<div class='gird gird-"+i+"-"+j+"'></div>");
+			gird.css({
+				"width": "75px",
+				"height": "75px", 
+				"float": "left",
+			});
+			chessboard.append(gird);
+		}
+	}
+}
+function drawPath(){
+	for(var i = 0;i < 8;i++){
+		for(var j = 0;j < 8;j++){
+			var gird = $(".gird-"+i+"-"+j);
+			if(path[i][j]==0){
+				gird.text(64);
+			}else{
+				gird.text(path[i][j]);
+			}
+		}
+	}
+}
 $(document).ready(function(){
+	generateGraph();
 	$(".newRandomBtn").click(function(){
 		init();
 	});
 	$(".nextAnswerBtn").click(function(){
 		solve();
+		drawPath();
 	});
-	/*solve();/*
-	for(var i = 0;i < resolution.length;i++){
-		for(var j = 0;j < 8;j++){
-			console.log(resolution[i][j]);
-		}
-	}*/
 });
