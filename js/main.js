b99cf55a1c1b5435b9e6e7e350bf376daedc6a2b
lj <jiezi19971225@163.com>
2018-01-05 01:27:26 +0800

    1.0

--- js/main.js
+++ js/main.js
@@ -17,201 +17,34 @@ stack={
 		this.data.length = 0;
 	}
 }
-//方向数组
-
-var mov=[
-	[-1,-2],
-	[-2,-1],
-	[-2,1],
-	[-1,2],
-	[1,2],
-	[2,1],
-	[2,-1],
-	[1,-2],
-];
-
-//路径数组
-var path=[
-	[],[],[],[],[],[],[],[]
-];
-//用于绘图的路径数组
-var pathToDraw=[
-	[],[],[],[],[],[],[],[]
-]
-//可行数数组
-var accessible=[
-	[],[],[],[],[],[],[],[]
-]
-//初始状态
-var beginStatu;
-//状态对象构造函数
-Statu=function(row,col,step){
+//左斜线
+var leftDiagonal = [];
+//右斜线
+var rightDiagonal = [];
+//列
+var colUsed = [];
+//方案
+var res = [];
+var cur = 0;
+//状态
+statu = function(row,cur,occupy){
 	this.row = row;
-	this.col = col;
-	this.step = step;
-	this.currentMoveWay = 0;
-	this.order= [];
+	this.cur = cur;
+	this.occupy = occupy;
 }
-var inAnima = false;
-var row63;
-var col63;
-var row64;
-var col64;
-//初始化函数
-function init(){
-	stack.clear();
-	initSupportArray();
-	beginStatu = generateRandomPositon();
-	stack.push(beginStatu);
-}
-//初始化辅助数组
 function initSupportArray(){
+	res.splice(0,res.length);
 	for(var i = 0;i < 8;i++){
-		for(var j = 0;j < 8;j++){
-			path[i][j] = 0;
-			accessible[i][j] = 0;
-			for(var k = 0;k < 8;k++){
-				var newI=i + mov[k][0];
-				var newJ=j + mov[k][1];
-				if(newI >= 0 && newI < 8 && newJ >= 0 && newJ < 8){
-					accessible[i][j]++;
-				} 
-			}
-		}
-	}	
-}
-//冒泡排序
-function bubbleSort(access,order){
-	for(var i = 0;i < 7;i++){
-		for(var j = 0;j < 7 - i;j++){
-			if(access[j]>access[j+1]){
-				var temp = order[j];
-				order[j] = order[j+1];
-				order[j+1] = temp;
-				temp = access[j];
-				access[j] = access[j+1];
-				access[j+1] = temp;
-			}
-		}
+		colUsed [i] = false;
 	}
-}
-//生成随机初始位置
-function generateRandomPositon(){
-	var rrow = Math.floor(Math.random()*8);
-	var rcol = Math.floor(Math.random()*8);
-	var beginStatu = new Statu(rrow,rcol,1);
-	handleStatuAccessible(beginStatu);
-	adjust(rrow,rcol);
-	path[rrow][rcol]=1;
-	$(".gird-"+rrow+"-"+rcol).text(1);
-	return beginStatu;
-}
-//处理状态的可行性数组
-function handleStatuAccessible(statu){
-	if(statu.step == 63){
-		for(var i = 0;i < 8;i++){
-			var newRow = statu.row + mov[i][0];
-			var newCol = statu.col + mov[i][1];
-			if(newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8 && !path[newRow][newCol]){
-				row63 = statu.row,col63 = statu.col,row64 = newRow,col64 = newCol;
-				return true;
-			}
-		}
-		return false;
+	for(var i = 0;i < 16;i++){
+		leftDiagonal[i] = rightDiagonal[i] = false; 
 	}
-
-
-	var access = [];
-	var order = [0,1,2,3,4,5,6,7];
-	for(var i = 0;i < 8;i++){
-		var newRow = statu.row + mov[i][0];
-		var newCol = statu.col + mov[i][1];
-		if(newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8 && !path[newRow][newCol]){
-				access[i] = accessible[newRow][newCol] - 1;
-		}else{
-			access[i] = 0;
-		}
-	}
-	bubbleSort(access,order);
-	var cur = 0;
-	while(access[cur] == 0 && cur < 8){
-		cur++;
-	}
-	statu.currentMoveWay = cur;
-	statu.order = order;
-
-	if(cur == 8){
-		return false;
-	}
-	return true;
-}
-//调整可行性
-function adjust(row,col){
-	for(var i = 0;i <8;i++){
-		var newRow = row + mov[i][0]; 
-		var newCol = col + mov[i][1];
-		if(newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8){
-			accessible[newRow][newCol]--;
-		} 
-	}
-}
-function rollBack(row,col){
-	for(var i = 0;i <8;i++){
-		var newRow = row + mov[i][0]; 
-		var newCol = col + mov[i][1];
-		if(newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8){
-			accessible[newRow][newCol]++;
-		} 
-	}
-}
-//生成一组答案
-function solve(){
-	var cnt = 0;
-
-	while(!stack.empty()){
-		var currentStatu = stack.top();
-		var haveNextStatu = 0;
-		console.log(currentStatu);
-		var currentMoveWay = currentStatu.currentMoveWay;
-
-		for(var i = currentStatu.currentMoveWay;i < 8;i++){
-			console.log(currentStatu);
-			moveMay = currentStatu.order[i];
-			var newRow = currentStatu.row + mov[moveMay][0];
-			var newCol = currentStatu.col + mov[moveMay][1];
-			var newStatu = new Statu(newRow,newCol,currentStatu.step+1);
-			if(currentStatu.step == 62){
-				for(var i = 0;i < 8;i++){
-					var lastRow = newRow + mov[i][0];
-					var lastCol = newCol + mov[i][1];
-					if(lastRow >= 0 && lastRow < 8 && lastCol >= 0 && lastCol < 8 && !path[lastRow][lastCol]){
-						console.log(lastRow,lastCol,path[lastRow][lastCol])
-						row63 = newRow,col63 = newCol,row64 = lastRow,col64 = lastCol;
-						currentStatu.currentMoveWay++;
-						return true;
-					}
-				}
-			}else if(handleStatuAccessible(newStatu)){
-				haveNextStatu = 1;
-				stack.push(newStatu);
-				adjust(newRow,newCol);
-				path[newRow][newCol] = currentStatu.step + 1;
-				currentStatu.currentMoveWay = i + 1;
-				break;	
-			}
-		}
-		if(!haveNextStatu){
-			path[currentStatu.row][currentStatu.col] = 0;
-			rollBack(currentStatu.row,currentStatu.col);
-			stack.pop();
-		}
-	}
-	return false;
 }
 function generateGird(){
 	var chessboard = $(".chessboard");
 	for(var i = 0;i < 8;i++){
-		for(var j=0;j < 8;j++){
+		for(var j = 0;j < 8;j++){
 			var gird = $("<div class='gird gird-"+i+"-"+j+"'></div>");
 			gird.css({
 				"width": "12.5%",
@@ -227,104 +60,107 @@ function generateGird(){
 		}
 	}
 }
-function removeGird(){
-	for(var i = 0;i < 8;i++){
-		for(var j = 0;j < 8;j++){
-			var gird=$(".gird-"+i+"-"+j);
-			gird.remove();
+
+function solve(){
+	var beginStatu = new statu(0,0,[]);
+	stack.push(beginStatu);
+	while(!stack.empty()){
+		var currentStatu = stack.top();
+		var haveNextStatu = false;
+		var currentRow = currentStatu.row;
+		var currentCur = currentStatu.cur;
+		if(currentStatu.row == 8){
+			res.push(currentStatu.occupy);
+			var lastPos = currentStatu.occupy[7];
+			colUsed[lastPos] = false;
+			leftDiagonal[ lastPos - 7 + 8] = false;
+			rightDiagonal[lastPos + 7] = false;
+			stack.pop();
+			continue;
+		}
+		for(var i = currentCur;i < 8;i++){
+			if(!leftDiagonal[i - currentRow + 8] && !rightDiagonal[i + currentRow] && !colUsed[i]){
+				haveNextStatu = true;
+				leftDiagonal[i - currentRow + 8] = true;
+				rightDiagonal[i + currentRow] = true;
+				colUsed[i] = true;
+				occupy = currentStatu.occupy.slice(0);
+				occupy [currentRow] = i;
+				var newStatu = new statu(currentRow + 1,0,occupy);
+				stack.push(newStatu);
+				currentStatu.cur = i + 1;
+				break;
+			}
+		}
+		if(!haveNextStatu){
+			var lastPos = currentStatu.occupy[currentStatu.row - 1];
+			colUsed[lastPos] = false;
+			leftDiagonal[lastPos - currentStatu.row + 1 + 8] = false;
+			rightDiagonal[lastPos + currentStatu.row - 1] = false;
+			stack.pop();
 		}
 	}
-	$(".horse").remove();
-}
-function drawPath(){
-	for(var i = 0;i < 8;i++){
-		for(var j = 0;j < 8;j++){
-			pathToDraw[i][j] = path[i][j];
-		}
-	}
-	pathToDraw[row63][col63] = 63;
-	pathToDraw[row64][col64] = 64;
-	for(var i = 0;i < 8;i++){
-		for(var j = 0;j < 8;j++){
-			var gird = $(".gird-"+i+"-"+j);
-			gird.text(pathToDraw[i][j]);
-		}
-	}
-}
-function rowOffset(row){
-	return row*75;
 }
 function colOffset(col){
 	return col*75;
 }
-function showAnima(){
-	inAnima = true;
-	var step = 1;
+function rowOffset(row){
+	return row*75;
+}
+function drawStragety(){
 	var chessboard = $(".chessboard");
-	var horse = $("<div class='horse'></div>");
-	var currentRow = beginStatu.row;
-	var currentCol = beginStatu.col;
-	chessboard.append(horse);
-	horse.css({
-		"position": "relative",
-		"height": "75px",
-		"width": "75px",
-		"background-image": "url('image/horse.jpg')",
-		"top": rowOffset(currentRow),
-		"left": colOffset(currentCol),
-	});
-	while(step < 64){
-		console.log(step);
-		for(var i = 0;i < 8;i++){
-			var nextRow = currentRow + mov[i][0];
-			var nextCol = currentCol + mov[i][1];
-			console.log(nextRow,nextCol);
-			if(nextRow >= 0 && nextRow < 8 && nextCol >= 0 && nextCol < 8 && pathToDraw[nextRow][nextCol] == step + 1){
-				if(step == 63){
-					horse.animate({
-					left: colOffset(nextCol),
-					top: rowOffset(nextRow)
-					},function(){
-						$(".showAnimaBtn").removeClass("disabled").attr("disabled",false);
-					});
-				}else{
-					horse.animate({
-					left: colOffset(nextCol),
-					top: rowOffset(nextRow)
-					});
-				}
-				
-				currentRow = nextRow;
-				currentCol = nextCol;
-				step++
-				break;
-			}
-		}
+	var stragety = res[cur];
+	console.log(stragety);
+	for(var i = 0;i < 8;i++){
+		var queen = $("<div class='queen queen-"+i+"'></div>");
+		chessboard.append(queen);
+		queen.css({
+			"left": colOffset(stragety[i]),
+			"top": rowOffset(i)
+		})
 	}
 }
 $(document).ready(function(){
 	generateGird();
-	init();
-	$(".restartBtn").click(function(){
-		removeGird();
-		generateGird();
-		init();
+	initSupportArray();
+	solve();
+	drawStragety(cur);
+	$(".number").text(cur+1);
+	$(".lastAnswerBtn").click(function(){
+		$(".queen").remove();
+		cur = (cur - 1 + 92) % 92;
+		$(".number").text(cur+1);
+		drawStragety();
+		$(".number").text(cur+1);
 	});
 	$(".nextAnswerBtn").click(function(){
-		$(".horse").remove();
-		if(!solve()){
-			alert("当前是最后一组解了");
-		}else{
-			drawPath();
-
-			$(".showAnimaBtn").removeClass("disabled").attr("disabled",false);
-
-		}
-
+		cur = (cur + 1) % 92;
+		$(".number").text(cur+1);
+		$(".queen").remove();
+		drawStragety();
 	});
-	$(".showAnimaBtn").click(function(){
-		$(".horse").remove();
-		$(".showAnimaBtn").addClass("disabled").attr("disabled",true);
-		showAnima();
+	$(".numberNAnswer").click(function(){
+		var number = $(".number-input").val();
+		if(parseInt(number) == number){
+			if(number >= 1 && number <= 92){
+				cur = number - 1;
+				$(".number").text(cur+1);
+				$(".queen").remove();
+				drawStragety();
+			}else{
+				alert("超出范围，输入无效！");
+				$(".number-input").val("");
+				return;
+			}
+		}else{
+			alert("请输入 1 - 92 间的正整数");
+			return;
+		}
+		$(".number-input").val("");
 	});
 });
+$("body").keydown(function(){
+	if(event.keyCode == "13"){
+		$(".numberNAnswer").click();
+	}
+})
